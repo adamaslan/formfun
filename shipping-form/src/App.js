e084e2d30487ffdb43d5592fe9722c2cfc4581b9
Adam Aslan <chillcoders@gmail.com>
2021-04-28 22:14:58 -0400

    5steps

--- shipping-form/src/App.js
+++ shipping-form/src/App.js
@@ -1,8 +1,11 @@
+
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Step1 } from "./Step1";
 import { Step2 } from "./Step2";
 import { Step3 } from "./Step3";
+import { Step4 } from "./Step4";
+import { Step5 } from "./Step5";
 import { Result } from "./Result";
 import { Header } from "./components/Header";
 
@@ -16,6 +19,9 @@ function App() {
           <Route exact path="/" component={Step1} />
           <Route path="/step2" component={Step2} />
           <Route path="/step3" component={Step3} />
+          <Route path="/step4" component={Step4} />
+          <Route path="/step5" component={Step5} />
+          
           <Route path="/result" component={Result} />
         </Switch>
       </Router>
